2b6ef6112c26c2bc2fb753c45bbb6c42d945cd86
Kyle <kyle@goodrx.com>
2015-12-14 20:34:03 -0800

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='pyramid_sendgrid_webhooks',
-    version='0.1.0',
+    version='1.0.0',
     description="Parses incoming Sendgrid Webhooks in Pyramid apps",
     long_description=readme + '\n\n' + history,
     author="Kyle Stark",
